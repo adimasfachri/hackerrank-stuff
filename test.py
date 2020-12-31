a09d63844deab2c55d6e91adf2894555a98724aa
adimasfachri <adimas_fachri@yahoo.com>
2020-12-31 21:33:58 +0700

    tambah vscode konfiguration & git ignore

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-print("Twinkle, twinkle, little star,")
-print("\tHow I wonder what you are! ")
-print("\t\tUp above the world so high,")
-print("\t\tLike a diamond in the sky.")
-print("Twinkle, twinkle, little star,")
-print("\tHow I wonder what you are")
+from math import pi
+x = float(input("jari2 lingkaran : "))
+
+print(pi*x**2)
